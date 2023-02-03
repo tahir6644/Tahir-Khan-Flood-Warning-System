b72d047b456652ba1dc306d541a29cb5eb5f4820
charu-maheshwari <cm2115@cam.ac.uk>
2023-02-03 21:12:05 +0000

    Task1B driver function - sort stations by distance

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,23 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+from .station import MonitoringStation
+
+def stations_by_distance(stations : MonitoringStation , p : tuple) -> list :
+    """
+    For a given list of stations and coordinate p, returns
+    a sorted list of station, distance tuple 
+    (based on the distance of the station from coordinate p) 
+    
+    """
+
+    station_distance = []
+    for station in stations:
+        distance = haversine(station.coord, p)
+        station_distance.append((station, distance))
+
+    station_distance = sorted_by_key(station_distance, 1)
+
+    return station_distance
+

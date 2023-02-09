214ddb89b7b395ef911e3c16a1a5ac3f49111b62
tahir6644 <tahirkhan6644@gmail.com>
2023-02-09 09:38:43 +0000

    Task1C demonstration

--- Task1C.py
+++ Task1C.py
@@ -1,5 +1,15 @@
+from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
+
 def run():
     # Put code here that demonstrates functionality
+    
+    CambridgeCityCenter = (52.2053, 0.1218)
+    stations = [i.name for i in stations_within_radius(build_station_list(), CambridgeCityCenter, 10.0)] # Within 10 km.
+
+    print(sorted(stations)) # sorted() arranges the names in alphabetical order.
+
+
 
 if __name__ == "__main__":
     run()
